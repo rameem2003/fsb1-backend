a99c46b2b217276d9277b52eed94b2996187e076
rameem2003 <rameem2019@gmail.com>
2026-06-08 22:23:00 +0600

    Update API specifications and data entries for CRUD operations

--- node-js/index.js
+++ node-js/index.js
@@ -29,6 +29,18 @@ const writeData = (data) => {
  */
 
 const server = http.createServer((req, res) => {
+  /**
+   * Get Data GET /data
+   */
+  if (req.url == "/data" && req.method == "GET") {
+    let data = readData();
+    res.statusCode = 200;
+    res.end(JSON.stringify(data));
+  }
+
+  /**
+   * Add Data POST /add
+   */
   if (req.url == "/add" && req.method == "POST") {
     let body = "";
 
@@ -52,8 +64,71 @@ const server = http.createServer((req, res) => {
       writeData(prevData);
       // console.log(body.toString());
 
+      res.statusCode = 201;
       res.end("Data received");
     });
+  }
+
+  /**
+   * Update Data PUT /update/:id
+   */
+  if (req.url.startsWith("/update/") && req.method == "PUT") {
+    let found = false;
+    let id = req.url.split("/")[2];
+
+    let body = "";
+
+    req.on("data", (chunk) => {
+      body = body + chunk;
+    });
+
+    req.on("end", () => {
+      let prevData = readData();
+      let newData = JSON.parse(body.toString());
+
+      let update = prevData.map((item) => {
+        if (item.id == id) {
+          found = true;
+          return {
+            ...item,
+            ...newData,
+          };
+        }
+
+        return item;
+      });
+
+      if (!found) {
+        res.statusCode = 404;
+        res.end("Data not found");
+        return;
+      }
+
+      writeData(update);
+      res.statusCode = 201;
+      res.end("Data Updated");
+    });
+  }
+
+  /**
+   * Delete Data DELETE /delete/:id
+   */
+
+  if (req.url.startsWith("/delete/") && req.method == "DELETE") {
+    let id = req.url.split("/")[2];
+
+    let prevData = readData();
+
+    let updatedData = prevData.filter((item) => item.id != id);
+    if (updatedData.length == prevData.length) {
+      res.statusCode = 404;
+      res.end("Data not found");
+      return;
+    }
+
+    writeData(updatedData);
+    res.statusCode = 200;
+    res.end("Data Deleted");
   }
 });
 
