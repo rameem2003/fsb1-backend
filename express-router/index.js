88c2b5e8b86dbc520e74e632e49388976ed588cc
Mahmood Hassan Rameem <rameem2019@gmail.com>
2026-07-01 22:00:14 +0600

    feat: initialize express server with custom routing and authentication middleware

--- express-router/index.js
+++ express-router/index.js
@@ -30,5 +30,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+  console.log("Server is running at http://localhost:5000");
 });
--- express-router/middlewares/middleware.js
+++ express-router/middlewares/middleware.js
@@ -1,6 +1,16 @@
 const middleware = (req, res, next) => {
-  console.log("Middlware Calling....");
-  next();
+  // console.log(req.headers.authorization.split(" ")[1]);
+  let token = req.headers.authorization.split(" ")[1];
+
+  if (token == "1234") {
+    next();
+    return;
+  } else {
+    return res.status(401).send({
+      success: false,
+      message: "Unauthorized",
+    });
+  }
 };
 
 export default middleware;
